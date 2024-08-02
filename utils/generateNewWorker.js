27394771e520f77184e69aade31c5f30f37baa88
Justin Gaba <jgaba@worthile.com>
2024-08-01 22:48:50 -0400

    Add consoles for debugging and defensive if requestTracker to help prevent errors

--- utils/generateNewWorker.js
+++ utils/generateNewWorker.js
@@ -2,16 +2,29 @@ const { Worker } = require('worker_threads');
 const path = require('path');
 const requestTracker = require('./requestTracker');
 
+
 const generateNewWorker = (workerName) => {
   const worker = new Worker(path.join(__dirname, '../workers', workerName));
   worker.on('message', (data) => {
     const { response, requestId } = data;
-    requestTracker[requestId](response);
-    delete requestTracker[requestId];
+ 
+    if (requestTracker[requestId]) {
+      requestTracker[requestId](response);
+      delete requestTracker[requestId];
+    }
+
   });
-  worker.on('error', () => {
+
+  worker.on('error', (err) => {
+    console.error(`Worker error: ${err.message}`);
     worker.terminate();
   });
+
+  worker.on('exit', (code) => {
+    console.log(`Worker exited with code: ${code}`);
+  });
+
+  console.log(`Created new worker: ${worker.threadId}`);
   return worker;
 }
 
